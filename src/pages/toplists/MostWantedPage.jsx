0218815b50867d031278fbc8648bd3a4b315ba2c
Elin Ahlgren <elin.ahlgren@hotmail.com>
2023-01-10 10:39:14 +0100

    user can see a toplist och the movies/series that most people want to see

--- src/pages/toplists/MostWantedPage.jsx
+++ src/pages/toplists/MostWantedPage.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import getOccurrences from '../../helpers/getOccurences'
+import getSortedOccurrences from '../../helpers/getSortedOccurrences'
+import useGetCollection from '../../hooks/useGetCollection'
 
 const MostWantedPage = () => {
+    const {data, loading} = useGetCollection('wishlist')
+    const [arraysOfItemAndCount, setArraysOfItemAndCount] = useState(null)
+
+    useEffect(() => {
+        const arrayOfTitles = data.map((doc) => doc.title)
+
+        const result = getOccurrences(arrayOfTitles)
+
+        const re = getSortedOccurrences([result[1], result[0]])
+        console.log(re)
+        setArraysOfItemAndCount(re)
+    }, [data])
+
     return (
         <div>
-            <h1>Most wanted</h1>
+            {loading && <p>loading...</p>}
+            {data && (
+                <>
+                    <h1>Most wanted</h1>
+                    {arraysOfItemAndCount && (
+                        <>
+                            {arraysOfItemAndCount[1].map((i, index) => (
+                                <p key={i}>{i} exists in {arraysOfItemAndCount[0][index]} wishlists</p>
+                            ))}
+                        </>
+                    )}
+                </>
+            )}
         </div>
     )
 }
