0a7f2a1e5adc5e3bcfb70a4c354f14e3530f4a4c
Elin Ahlgren <elin.ahlgren@hotmail.com>
2023-01-20 13:24:36 +0100

    if no icon in CardLink- do not show <img>

--- src/components/CardLink.jsx
+++ src/components/CardLink.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import { useNavigate } from 'react-router-dom'
 
-const CardLink = ({ endpoint, icon, text }) => {
+const CardLink = ({ endpoint, icon = null, text }) => {
     const navigate = useNavigate()
 
     return (
         <div onClick={() => navigate(endpoint)} className='card-link'>
-            <img src={icon} alt="icon" />
+            {icon && <img src={icon} alt="icon" />}
             <h3>{text}</h3>
         </div>
     )
